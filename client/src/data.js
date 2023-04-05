343d76bb053460744a3d68aed4eda201be316505
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-04-05 01:10:22 -0300

    att

--- client/src/data.js
+++ client/src/data.js
@@ -2,15 +2,15 @@
 export const posts = [
     {
         id: 1,
-        username: "Roger",
-        name: "Roger P",
+        username: "joao",
+        name: "João",
         userImg: "https://images.pexels.com/photos/163036/mario-luigi-yoschi-figures-163036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         postImg: "https://images.pexels.com/photos/440731/pexels-photo-440731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
         id: 2,
-        username: "Maria",
-        name: "Maria P",
+        username: "maria",
+        name: "Maria",
         userImg: "https://images.pexels.com/photos/9646287/pexels-photo-9646287.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         postImg: "https://images.pexels.com/photos/1804177/pexels-photo-1804177.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
