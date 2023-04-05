3bf5bf12f45e4f163b8acedf47e0cbadad6917be
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-04-05 01:00:04 -0300

    att

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -5,34 +5,62 @@ import Menssage from "../../img/mail1.svg"
 import Settings from "../../img/definicoes1.svg"
 import { useEffect, useState } from "react"
 
-const Navbar = ({socket}) => {
+const Navbar = ({ socket }) => {
     const [notifications, setNotifications] = useState([])
+    const [open, setOpen] = useState(false)
 
     useEffect(() => {
         socket.on("getNotification", (data) => {
             setNotifications((prev) => [...prev, data])
         })
-    },[socket])
+    }, [socket])
 
     console.log(notifications)
+
+    const displayNotification = ({ senderName, type }) => {
+        let action
+
+        if (type === 1) {
+            action = "curtiu"
+        } else if (type === 2) {
+            action = "comentou"
+        } else {
+            action = "compartilhou"
+        }
+        return (
+            <span className="notification">{`${senderName} ${action} seu post`}</span>
+        )
+    }
+
+    const handleRead = () => {
+        setNotifications([])
+        setOpen(false)
+    }
 
     return (
         <div className="navbar">
             <span className="logo">Notificação App</span>
             <div className="icons">
-                <div className="icon">
+                <div className="icon" onClick={() => setOpen(!open)}>
                     <img src={Notification} alt="" className="iconImg" />
-                    <div className="counter">3</div>
+                    {
+                        notifications.length > 0 &&
+                    <div className="counter">{notifications.length}</div>
+                    }
                 </div>
-                <div className="icon">
+                <div className="icon" onClick={() => setOpen(!open)}>
                     <img src={Menssage} alt="" className="iconImg" />
-                    <div className="counter">3</div>
                 </div>
-                <div className="icon">
+                <div className="icon" onClick={() => setOpen(!open)}>
                     <img src={Settings} alt="" className="iconImg" />
-                    <div className="counter">3</div>
                 </div>
             </div>
+            {open && (
+                <div className="notifications">
+                    {notifications.map((n) => displayNotification(n))}
+                    <button className="nButton" onClick={handleRead}>Marcar como já lido</button>
+                </div>
+            )}
         </div>
     )
 }
